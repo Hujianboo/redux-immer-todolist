ec21c2ed2f883311eeabb3cafe27ffb0f909530a
Hujianboo <466636709@qq.com>
2022-08-30 21:46:46 +0800

    fix: fix move fast problem

--- src/components/CloseIcon.tsx
+++ src/components/CloseIcon.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { XCircleIcon as XCircleIconSolid } from '@heroicons/react/24/solid'
 import { XCircleIcon as XCircleIconOutline } from '@heroicons/react/24/outline'
 const CloseIcon = (
@@ -8,14 +8,37 @@ const CloseIcon = (
     }
 ) => {
     const [hovered,setHovered] = useState(false);
+    const closeRef = useRef()
+
+    const checkHover = (e => {
+      if (closeRef.current) {
+        const mouseOver = closeRef.current.contains(e.target);
+        if (!hovered && mouseOver) {
+          console.log(hovered,mouseOver);
+          setHovered(true)
+        }
+  
+        if (hovered && !mouseOver) {
+          setHovered(false)
+        }
+      }
+    });
+    useEffect(() => {
+      window.addEventListener("mousemove", checkHover, true);
+      return () => {
+        window.removeEventListener("mousemove", checkHover, true);
+      }
+    },[hovered])
     return (
         <div 
             id='123'
             className='w-[24px] h-[24px]'
-            onMouseOver={()=>{setHovered(true)}}
-            onMouseLeave={()=>{setHovered(false)}}    
+            onMouseEnter={(e)=>{e.preventDefault();setHovered(true)}}
+            onMouseLeave={(e)=>{e.preventDefault();setHovered(false)}}
         >  
-          <div>
+          <div 
+            ref={closeRef}
+          >
             {hovered ?
             <XCircleIconSolid className={className} onClick={onClick}/>: <XCircleIconOutline className={className} onClick={onClick}/> 
             }
